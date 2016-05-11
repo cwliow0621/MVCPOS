3c66831477d9ef67131583edf66e5e24510d1126
ken88ling <ken88ling@gmail.com>
2016-05-11 22:36:18 +1000

    Code first for customer table

---
+++ MVC_POS_EntityFrameWork.DataAccess/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_POS_EntityFrameWork.DataAccess.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
